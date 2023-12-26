52d958bfd538faf346c8bca4a232a4c34aa315b6
Daniel <carreraprogrammer@gmail.com>
2023-12-26 09:24:23 -0500

    Add initializeDb.ts for GraphQL replication

---
+++ src/db/initializeDb.ts
@@ -0,0 +1,133 @@
+import { todoSchema } from './schemas/todoSchema';
+
+import { addRxPlugin } from 'rxdb';
+import {
+  replicateGraphQL
+} from 'rxdb/plugins/replication-graphql';
+
+
+// TODO import these only in non-production build
+
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
+addRxPlugin(RxDBDevModePlugin);
+import { wrappedValidateAjvStorage } from 'rxdb/plugins/validate-ajv';
+
+import { RxDBUpdatePlugin } from 'rxdb/plugins/update';
+addRxPlugin(RxDBUpdatePlugin);
+
+import { RxDBQueryBuilderPlugin } from 'rxdb/plugins/query-builder';
+addRxPlugin(RxDBQueryBuilderPlugin);
+
+import { RxDBLeaderElectionPlugin } from 'rxdb/plugins/leader-election';
+addRxPlugin(RxDBLeaderElectionPlugin);
+
+//types
+
+import { createRxDatabase } from 'rxdb';
+import { getRxStorageDexie } from 'rxdb/plugins/storage-dexie';
+
+const pullQueryBuilder = (checkpoint, limit) => {
+  if (!checkpoint || checkpoint === null) {
+    checkpoint = {
+      id: "0",
+      updatedAt: "1970-01-01T00:00:00Z"
+    };
+  }
+  
+  const query = `query SyncTodos($checkpoint: CheckpointInput, $limit: Int) {
+    syncTodos(checkpoint: $checkpoint, limit: $limit) {
+      documents {
+        id
+        text
+        isCompleted
+        deleted
+        createdAt
+        updatedAt
+      }
+      checkpoint {
+        id
+        updatedAt
+      }
+    }
+  }  
+  `;
+  
+  return {
+    query,
+    operationName: 'SyncTodos',
+    variables: {
+      checkpoint,
+      limit
+    }
+  };
+};
+
+const syncURL = 'http://localhost:3000/graphql';
+const batchSize = 50;
+
+export class GraphQLReplicator {
+  private db;
+  private replicationState;
+
+  constructor(db) {
+      this.db = db;
+      this.replicationState = null;
+  }
+
+  async restart(): Promise<void> {
+      if (this.replicationState) {
+          this.replicationState.cancel();
+      }
+
+      this.replicationState = this.setupGraphQLReplication();
+  }
+
+  private setupGraphQLReplication() {
+    const replicationState = replicateGraphQL({
+      collection: this.db.todos,
+      url: {
+        http: syncURL,
+      },
+      pull: {
+        queryBuilder: pullQueryBuilder,
+        batchSize: 50,
+      },
+      deletedField: 'deleted',
+      live: false,
+      replicationIdentifier: 'my-replication'
+    });
+  
+    replicationState.error$.subscribe(err => {
+      console.error('replication error:');
+      console.dir(err);
+    });
+  
+    console.log('Replication was successfully setup');
+  
+    return replicationState;
+  }
+
+  getReplicationState() {
+    return this.replicationState;
+  }
+}
+
+export const createDb = async () => {
+  console.log('DatabaseService: creating database..');
+
+  const db = await createRxDatabase({
+      name: 'tododatabase',
+      storage: getRxStorageDexie(),
+      ignoreDuplicate: true
+  });
+
+  console.log('DatabaseService: created database');
+
+  await db.addCollections({
+      todos: {
+          schema: todoSchema,
+      }
+  });
+
+  return db;
+};
