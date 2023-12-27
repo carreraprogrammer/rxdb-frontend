c6317b628f67254353cdf217cb0f541de3c0f6c0
Daniel <carreraprogrammer@gmail.com>
2023-12-27 15:24:06 -0500

    Refactor db replication logic in DbContext component

--- src/components/dbContext.tsx
+++ src/components/dbContext.tsx
@@ -7,21 +7,24 @@ export const useDb = () => useContext(DbContext);
 
 export const DbProvider = ({ children }) => {
   const [db, setDb] = useState(null);
-  const [isReplicating, setIsReplicating] = useState(true);
+  const [isFirstLoad, setIsFirstLoad] = useState(true);
 
   useEffect(() => {
-      const initDb = async () => {
-        const db = await createDb();
+    const initDb = async () => {
+      const db = await createDb();
+      // Sólo inicia la replicación si es la primera carga
+      if (isFirstLoad) {
         const replicator = new GraphQLReplicator(db);
         await replicator.restart();
-        setDb(db);
-        setIsReplicating(false);
+        setIsFirstLoad(false); // Actualiza el estado después de la replicación
       }
+      setDb(db);
+    };
 
-      initDb();
-  }, []); // Dependencias vacías para que se ejecute solo en el montaje
+    initDb();
+  }, [isFirstLoad]); // Dependencia: ejecuta el efecto si isFirstLoad cambia
 
-  if (isReplicating) {
+  if (!db) {
     return <div>Replicando base de datos...</div>;
   }
 
